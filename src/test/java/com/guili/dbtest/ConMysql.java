8c1ff1eb3f449fcda1b748c86d5adae5c9fa78f0
xiaozhengdong <770154651@qq.com>
2013-11-07 00:10:08 +0800

    mysql测试

--- src/test/java/com/guili/dbtest/ConMysql.java
+++ src/test/java/com/guili/dbtest/ConMysql.java
@@ -10,12 +10,12 @@ public class ConMysql {
     }
 
     private Connection conn = null;
-    private String url = "jdbc:sqlserver://192.168.0.103:1433;databaseName=ecshop";
-    private String user = "sa";
+    private String url = "jdbc:mysql://127.0.0.1:3306/ecshop";
+    private String user = "root";
 
     public Connection getconn() {
         try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
             conn = DriverManager.getConnection(url, user, "123456");
             return conn;
         } catch (Exception e) {
