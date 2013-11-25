a8bce920e0ef3a8b71a7e349d17709e090a867c8
dongzi <770154651@qq.com>
2013-11-25 15:55:52 +0800

    analyze pinpai

--- src/main/java/org/guili/ecshop/business/impl/evaluate/AnalyzeTmallList.java
+++ src/main/java/org/guili/ecshop/business/impl/evaluate/AnalyzeTmallList.java
@@ -7,10 +7,18 @@ import org.apache.log4j.Logger;
 import org.guili.ecshop.util.CommonTools;
 import org.guili.ecshop.util.SpiderRegex;
 
+/**
+ * 分析淘宝品牌及品牌下的商品
+ * @ClassName:   AnalyzeTmallList 
+ * @Description: 分析淘宝品牌及品牌下的商品(这里用一句话描述这个类的作用) 
+ * @author:      guilige 
+ * @date         2013-11-25 下午3:24:42 
+ *
+ */
 public class AnalyzeTmallList {
 	
 	private static Logger logger=Logger.getLogger(AnalyzeTmallList.class);
-
+	
 	/**
 	 * 根据商品列表链接获得商品详细页链接列表
 	 * @param url	商品全部查询链接
@@ -73,6 +81,9 @@ public class AnalyzeTmallList {
 				if(myBrandItem.contains("&")){
 					myBrandItem=myBrandItem.substring(0, myBrandItem.indexOf("&"));
 				}
+				if(!myBrandItem.contains("id=")){
+					continue;
+				}
 				if(!onePageBrandItems.contains(myBrandItem)){
 					onePageBrandItems.add(myBrandItem);
 					logger.debug("onePageBrandItems has :"+myBrandItem);
@@ -106,10 +117,90 @@ public class AnalyzeTmallList {
 		}
 		return totalpage;
 	}
+	
+	/**
+	 * 分析天猫品牌列表
+	 * @param url 
+	 * @return  返回list like：http://uniqlo.tmall.com/
+	 */
+	public static List<String> AnalyzeTmallBrand(String url){
+		List<String> tmallbrandList=new ArrayList<String>();
+		//正则解析器
+		SpiderRegex regex = new SpiderRegex();
+		String htmltext=CommonTools.requestUrl(url, "gbk");
+		if(htmltext.equals("")){
+			return null;
+		}
+		//String brandItemRegex = "target=\"_blank\" href=\"(.*?)\" title=\"";
+		String brandItemRegex = "<a target=\"_blank\" class=\"bFlis-con-mask\" href=\"(.*?)\">";
+		String[] bigbrandItem = regex.htmlregex(htmltext,brandItemRegex,true);
+		if(bigbrandItem!=null && bigbrandItem.length>0){
+			for(String brandItem:bigbrandItem){
+				brandItem=AnalyzeTmallList.convertBrandItem(brandItem);
+				if(brandItem.equals("")){
+					continue;
+				}
+				if(tmallbrandList.contains(brandItem)){
+					continue;
+				}
+				tmallbrandList.add(brandItem);
+			}
+		}
+		//查看小的商标
+		brandItemRegex = "<a class=\"bFlil-link\" target=\"_blank\" href=\"(.*?)\" title=";
+		String[] brandItems = regex.htmlregex(htmltext,brandItemRegex,true);
+		if(brandItems!=null && brandItems.length>0){
+			for(String brandItem:brandItems){
+				brandItem=AnalyzeTmallList.convertBrandItem(brandItem);
+				if(brandItem.equals("")){
+					continue;
+				}
+				if(tmallbrandList.contains(brandItem)){
+					continue;
+				}
+				tmallbrandList.add(brandItem);
+			}
+		}
+		//查看特殊排列商标
+		brandItemRegex = "<a class=\"bFlil-link\" target=\"_blank\" title=\"(.*?)\">";
+		String[] brandItem1 = regex.htmlregex(htmltext,brandItemRegex,true);
+		if(brandItem1!=null && brandItem1.length>0){
+			for(String brandItem:brandItem1){
+				//判断当前是否有品牌链接
+				if(brandItem.indexOf("href=\"")>0){
+					brandItem=brandItem.substring(brandItem.indexOf("href=\"")+"href=\"".length());
+				}
+				brandItem=AnalyzeTmallList.convertBrandItem(brandItem);
+				if(brandItem.equals("")){
+					continue;
+				}
+				if(tmallbrandList.contains(brandItem)){
+					continue;
+				}
+				tmallbrandList.add(brandItem);
+			}
+		}
+		return tmallbrandList;
+	}
+	
+	/**
+	 * 转换brandItem
+	 */
+	public static String convertBrandItem(String brandItem){
+		if(brandItem.contains("list.tmall.com")){
+			return "";
+		}
+		if(brandItem.indexOf("?")>0){
+			brandItem=brandItem.substring(0, brandItem.indexOf("?"));
+		}
+		brandItem=brandItem.substring(0,brandItem.indexOf("com/")+"com/".length());
+		return brandItem;
+	}
 	//test
 	public static void main(String[] args) {
 		logger.debug(AnalyzeTmallList.convertUrl("http://hanlidu.tmall.com/shop/view_shop.htm?spm=a220m.1000862.1000730.2.fhTbDq&user_number_id=728412204&rn=f2b6ed1084b76c27501189515f9279f2"));
-		AnalyzeTmallList.getBrandItemsList("http://uniqlo.tmall.com/search.htm?search=y");
+		//AnalyzeTmallList.getBrandItemsList("http://timberland.tmall.com/?spm=a3200.2787281.a2223nt.7.F0V2tu");
+		AnalyzeTmallList.AnalyzeTmallBrand("http://brand.tmall.com/brandMap.htm?spm=a3200.2192433.0.0.RIdKpk");
 	}
 	
 }
